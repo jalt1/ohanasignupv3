06c7d61ff813d5d42335e91b1b09621d86931e02
jalt1 <jocelyn@ohanaparents.com>
2013-11-20 17:13:03 -0600

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ohanasignupv3::Application.config.secret_key_base = 'afbb0ecbbf205fd7afd9b0604d1394119ddc7bbc3078ba4659b53db9be39a0869fd78d65a6a377c813173f1d70f504a6cfca8935c698f38a206510100133c4b7'
